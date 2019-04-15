0e85ae18545275896e359a620e08a5dd86ce5437
Laurent <laurent.laligant2018@campus-eni.fr>
2019-04-15 13:56:35 +0200

    initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends Controller
+{
+        /**
+         * @Route("/", name="home")
+         */
+        public function index()
+        {
+            return $this->render('main/index.html.twig', [
+                'controller_name' => 'MainController',
+            ]);
+        }
+    /**
+     * @Route("/fail")
+     * */
+    public function fail() {
+        echo "helloworld";
+
+    }
+        /**
+         * @Route("/success", name="success")
+         */
+            public function success()
+            {
+                return new Response('<html><body>Hello world ! kiss</body></html>');
+
+                }
+        /**
+        * @Route("/hello")
+        * */
+            public function helloWorld()
+            {
+              return  $this->render('main/hello.html.twig');
+            }
+    /**
+     * @Route("/securite")
+     */
+    public function securite(Request $request)
+    {
+       $comment= $request->get('comment');
+        return $this->render(
+            'main/securite.html.twig',
+        compact('comment')
+    );
+    }
+    }
+
