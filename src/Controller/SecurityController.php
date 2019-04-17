ac6b7e10753d69c09c1ffee268e073fdb5d7b43f
Laurent <laurent.laligant2018@campus-eni.fr>
2019-04-17 13:34:04 +0200

    se souvenir

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,6 +18,7 @@ class SecurityController extends Controller
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+        $this->addFlash("warning",$error);
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
